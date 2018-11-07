d4825229fd3c12be9cc69deb940aa6b6a3bd818d
Gail Sark <virginia.sark@platotesting.com>
2018-11-07 08:11:05 -0400

    java

--- HelloGail.java
+++ HelloGail.java
@@ -1,1 +1,8 @@
 console log "Hello Gail"; 
+
+public class HelloGail {
+	public static void main(String[] args) {
+	// Prints "Hello, Virgina Sark" in the terminal window.
+	System.out.println("Hello Gail"); 
+}
+}
